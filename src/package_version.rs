091d1cea4a6175a5c51e61c44ab64f995869fbee
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2022-08-16 13:10:24 +0200

    Formatted package_version.rs
    - Fixed naming
        - `VERSION_VALIDATIOR` to `VERSION_VALIDATOR`
        - `ReleaseCanidate` to `ReleaseCandidate`
    - Did some manual formatting
    - Ran cargo fmt

--- src/package_version.rs
+++ src/package_version.rs
@@ -1,97 +1,98 @@
-//! # Handling of pep-440
-//! 
-//! This module implements Pythons Package versioning system read more at <https://peps.python.org/pep-0440/>
+//! # Handling of `PEP-440`
+//!
+//! This module implements Pythons Package versioning system.
+//! Read more at <https://peps.python.org/pep-0440/>
 use anyhow::Result;
 use lazy_static::lazy_static;
 use pomsky_macro::pomsky;
 use serde::{Deserialize, Serialize};
 use std::{cmp::Ordering, fmt};
 
-
-/// Rulex version of 
-/// Python's pep-440 Regex (<https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions>)
+/// Rulex version of
+/// Python's PEP-440 Regex
+/// (<https://peps.python.org/pep-0440/#appendix-b-parsing-version-strings-with-regular-expressions>)
 static VALIDATION_REGEX: &str = pomsky!(
-// Version String may start with v<version_number>
-// Example:
-// v1.0
-"v"?
+    // Version String may start with v<version_number>
+    // Example:
+    // v1.0
+    "v"?
 
-// Version String may include an epoch <epoch_num>!<version>
-// Example:
-// 1!1.0
-(:epoch(['0'-'9']+)'!')?
+    // Version String may include an epoch <epoch_num>!<version>
+    // Example:
+    // 1!1.0
+    (:epoch(['0'-'9']+)'!')?
 
-// Version String must include major and minor version <major>.<minor>
-// Example:
-// 1.0
-:release(['0'-'9']+("."['0'-'9']+)*)
+    // Version String must include major and minor version <major>.<minor>
+    // Example:
+    // 1.0
+    :release(['0'-'9']+("."['0'-'9']+)*)
 
-// Version String may include Pre-Header
-// Example:
-// 1.0.preview-2
-// 1.0.rc2
-// 1.0beta2
-:pre(
-    ["-" "_" "."]?
+    // Version String may include Pre-Header
+    // Example:
+    // 1.0.preview-2
+    // 1.0.rc2
+    // 1.0beta2
+    :pre(
+        ["-" "_" "."]?
 
-    :pre_l(
-    ("preview"|"alpha"|"beta"|"pre"|"rc"|"a"|"b"|"c")
+        :pre_l(
+        ("preview"|"alpha"|"beta"|"pre"|"rc"|"a"|"b"|"c")
+        )
+
+        ["-" "_" "."]?
+
+        :pre_n(['0'-'9']+)?
+    )?
+
+    // Version String may include Post-Header
+    // Examples:
+    // 1.0-9
+    // 1.0-post2
+    // 1.0.post.2
+    :post(
+        "-"
+        :post_n1(['0'-'9']+)
+
+        |
+
+        ["-" "_" "."]?
+        :post_l("post" | "rev" | "r")
+        ["-" "_" "."]?
+        :post_n2(['0'-'9']+)?
+    )?
+
+    // Version string may include Dev-header
+    // Example:
+    // 1.0-dev3
+    // 1.0dev4
+    // 1.0_dev_9
+    :dev(
+        ["-" "_" "."]?
+        :dev_l("dev")
+        ["-" "_" "."]?
+        :dev_n(['0'-'9']+)?
+    )?
+
+    // Version string may include Local Version
+    // Local version must start with +
+    // Example:
+    // 1.0+this.can.say.anything.as.long.as.its.a.letter.or.number.231241
+    (
+    "+"
+    :local(
+        ['a'-'z' '0'-'9']+
+        ((["-" "_" "."] ['a'-'z' '0'-'9']+)+)?
     )
-
-    ["-" "_" "."]?
-
-    :pre_n(['0'-'9']+)?
-)?
-
-// Version String may include Post-Header
-// Examples:
-// 1.0-9
-// 1.0-post2
-// 1.0.post.2
-:post(
-    "-"
-    :post_n1(['0'-'9']+)
-
-    |
-
-    ["-" "_" "."]?
-    :post_l("post" | "rev" | "r")
-    ["-" "_" "."]?
-    :post_n2(['0'-'9']+)?
-)?
-
-// Version string may include Dev-header
-// Example:
-// 1.0-dev3
-// 1.0dev4
-// 1.0_dev_9
-:dev(
-    ["-" "_" "."]?
-    :dev_l("dev")
-    ["-" "_" "."]?
-    :dev_n(['0'-'9']+)?
-)?
-
-// Version string may include Local Version
-// Local version must start with +
-// Example:
-// 1.0+this.can.say.anything.as.long.as.its.a.letter.or.number.231241
-(
-"+"
-:local(
-    ['a'-'z' '0'-'9']+
-    ((["-" "_" "."] ['a'-'z' '0'-'9']+)+)?
-)
-)?
+    )?
 );
 
-/// # Pep-440 Developmental release identifier
+/// # `PEP-440` Developmental release identifier
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, PartialOrd)]
 pub struct DevHead {
     dev_num: Option<u32>,
 }
 
-/// Pep-440 Post-Release Identifier Keyword
+/// `PEP-440` Post-Release Identifier Keyword
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub enum PostHead {
     Post,
@@ -104,7 +105,7 @@ impl PartialOrd for PostHead {
     }
 }
 
-/// # Pep-440 Post-Release identifier
+/// # `PEP-440` Post-Release identifier
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
 pub struct PostHeader {
     pub post_head: Option<PostHead>,
@@ -131,7 +132,7 @@ impl PartialOrd for PostHeader {
     }
 }
 
-/// # Pep-440 Pre-Release identifier
+/// # `PEP-440` Pre-Release identifier
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, PartialOrd)]
 pub enum PreHeader {
     /// Present in versions like 1.1beta1 or 1.0b1 both are represented the same way
@@ -151,12 +152,12 @@ pub enum PreHeader {
     Preview(Option<u32>),
     /// Present in versions like 1.1-rc-4 or 1.1c-4
     /// ```
-    /// PreHeader::ReleaseCanidate(Some(4))
+    /// PreHeader::ReleaseCandidate(Some(4))
     /// ```
-    ReleaseCanidate(Option<u32>),
+    ReleaseCandidate(Option<u32>),
 }
 
-/// Pep-440 Release numbers
+/// `PEP-440` Release numbers
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, PartialOrd)]
 pub struct ReleaseHeader {
     /// Major release such as 1.0 or breaking changes
@@ -165,10 +166,10 @@ pub struct ReleaseHeader {
     pub minor: u32,
 }
 
-/// Pep-440 Compliant versioning system
-/// 
+/// `PEP-440` Compliant versioning system
+///
 /// This struct is sorted so that PartialOrd
-/// corretly interpets priority
+/// correctly interprets priority
 ///
 /// Lower == More important
 ///
@@ -182,7 +183,7 @@ pub struct PackageVersion {
     #[derivative(PartialOrd = "ignore", PartialEq = "ignore")]
     pub original: String,
 
-    /// # Pep-440 Local version identifier
+    /// # `PEP-440` Local version identifier
     /// Local version sorting will have to be it's own issue
     /// since there are no limits to what a local version can be
     ///
@@ -197,19 +198,19 @@ pub struct PackageVersion {
     #[derivative(PartialOrd = "ignore", PartialEq = "ignore")]
     pub local: Option<String>,
 
-    /// # Pep-440 Developmental release identifier
+    /// # `PEP-440` Developmental release identifier
     pub dev: Option<DevHead>,
 
-    /// # Pep-440 Post-Release identifier
+    /// # `PEP-440` Post-Release identifier
     pub post: Option<PostHeader>,
 
-    /// # Pep-440 Pre-Release identifier
+    /// # `PEP-440` Pre-Release identifier
     pub pre: Option<PreHeader>,
 
-    /// # Pep-440 Release number
+    /// # `PEP-440` Release number
     pub release: ReleaseHeader,
 
-    /// # Pep-440 Version-Epoch
+    /// # `PEP-440` Version-Epoch
     pub epoch: Option<u32>,
 }
 
@@ -218,7 +219,7 @@ impl PackageVersion {
         lazy_static! {
             // Safe to unwrap since Regex is predefined
             // Regex as defined in PEP-0440
-            static ref VERSION_VALIDATIOR: regex::Regex =
+            static ref VERSION_VALIDATOR: regex::Regex =
                 regex::Regex::new(VALIDATION_REGEX).unwrap();
         }
 
@@ -226,7 +227,7 @@ impl PackageVersion {
         // Groups are:
         // epoch, release, pre, pre_l, pre_n, post, post_l, post_n1, post_n2,
         // dev, dev_l, dev_n, local
-        let version_match = match VERSION_VALIDATIOR.captures(version) {
+        let version_match = match VERSION_VALIDATOR.captures(version) {
             Some(v) => v,
             None => anyhow::bail!("Failed to decode version {}", version),
         };
@@ -265,14 +266,14 @@ impl PackageVersion {
                 };
 
                 // Should be safe to unwrap since we already checked if pre has a value
-                // since pre_n has to exist 
+                // since pre_n has to exist
                 match version_match.name("pre_l").unwrap().as_str() {
                     "alpha" => Some(PreHeader::Alpha(pre_n)),
                     "a" => Some(PreHeader::Alpha(pre_n)),
                     "beta" => Some(PreHeader::Beta(pre_n)),
                     "b" => Some(PreHeader::Beta(pre_n)),
-                    "rc" => Some(PreHeader::ReleaseCanidate(pre_n)),
-                    "c" => Some(PreHeader::ReleaseCanidate(pre_n)),
+                    "rc" => Some(PreHeader::ReleaseCandidate(pre_n)),
+                    "c" => Some(PreHeader::ReleaseCandidate(pre_n)),
                     "preview" => Some(PreHeader::Preview(pre_n)),
                     "pre" => Some(PreHeader::Preview(pre_n)),
                     _ => None,
@@ -358,7 +359,7 @@ mod tests {
     use super::PreHeader;
     use super::ReleaseHeader;
 
-    fn check_a_greater<T>(a: T, b: T) -> Result<()>
+    fn test_a_greater<T>(a: T, b: T) -> Result<()>
     where
         T: PartialEq + PartialOrd + Debug,
     {
@@ -373,8 +374,8 @@ mod tests {
     }
 
     #[test]
-    fn check_pep440_ordering() -> Result<()> {
-        check_a_greater(
+    fn test_pep440_ordering() -> Result<()> {
+        test_a_greater(
             PackageVersion::new("v1!1.0-preview-921.post-516.dev-241+yeah.this.is.the.problem.with.local.versions")?,
             PackageVersion::new("1.0")?,
         )?;
@@ -382,16 +383,16 @@ mod tests {
     }
 
     #[test]
-    fn check_release_ordering() -> Result<()> {
-        check_a_greater(
+    fn test_release_ordering() -> Result<()> {
+        test_a_greater(
             ReleaseHeader { major: 1, minor: 0 },
             ReleaseHeader { major: 0, minor: 0 },
         )?;
-        check_a_greater(
+        test_a_greater(
             ReleaseHeader { major: 1, minor: 1 },
             ReleaseHeader { major: 1, minor: 0 },
         )?;
-        check_a_greater(
+        test_a_greater(
             ReleaseHeader { major: 2, minor: 1 },
             ReleaseHeader {
                 major: 1,
@@ -402,29 +403,29 @@ mod tests {
     }
 
     #[test]
-    fn check_pre_ordering() -> Result<()> {
-        check_a_greater(PreHeader::ReleaseCanidate(None), PreHeader::Preview(None))?;
-        check_a_greater(PreHeader::Preview(None), PreHeader::Alpha(None))?;
-        check_a_greater(PreHeader::Alpha(None), PreHeader::Beta(None))?;
+    fn test_pre_ordering() -> Result<()> {
+        test_a_greater(PreHeader::ReleaseCandidate(None), PreHeader::Preview(None))?;
+        test_a_greater(PreHeader::Preview(None), PreHeader::Alpha(None))?;
+        test_a_greater(PreHeader::Alpha(None), PreHeader::Beta(None))?;
 
-        check_a_greater(
-            PreHeader::ReleaseCanidate(Some(2)),
-            PreHeader::ReleaseCanidate(Some(1)),
+        test_a_greater(
+            PreHeader::ReleaseCandidate(Some(2)),
+            PreHeader::ReleaseCandidate(Some(1)),
         )?;
-        check_a_greater(PreHeader::Preview(Some(50)), PreHeader::Preview(Some(3)))?;
-        check_a_greater(PreHeader::Alpha(Some(504)), PreHeader::Alpha(Some(0)))?;
-        check_a_greater(PreHeader::Beta(Some(1234)), PreHeader::Beta(Some(1)))?;
+        test_a_greater(PreHeader::Preview(Some(50)), PreHeader::Preview(Some(3)))?;
+        test_a_greater(PreHeader::Alpha(Some(504)), PreHeader::Alpha(Some(0)))?;
+        test_a_greater(PreHeader::Beta(Some(1234)), PreHeader::Beta(Some(1)))?;
 
-        check_a_greater(
-            PreHeader::ReleaseCanidate(Some(1)),
+        test_a_greater(
+            PreHeader::ReleaseCandidate(Some(1)),
             PreHeader::Beta(Some(45067885)),
         )?;
         Ok(())
     }
 
     #[test]
-    fn check_post_ordering() -> Result<()> {
-        check_a_greater(
+    fn test_post_ordering() -> Result<()> {
+        test_a_greater(
             PostHeader {
                 post_head: Some(PostHead::Post),
                 post_num: Some(0),
@@ -434,7 +435,7 @@ mod tests {
                 post_num: None,
             },
         )?;
-        check_a_greater(
+        test_a_greater(
             PostHeader {
                 post_head: Some(PostHead::Post),
                 post_num: Some(1),
@@ -448,14 +449,14 @@ mod tests {
     }
 
     #[test]
-    fn check_dev_ordering() -> Result<()> {
-        check_a_greater(DevHead { dev_num: Some(0) }, DevHead { dev_num: None })?;
-        check_a_greater(DevHead { dev_num: Some(1) }, DevHead { dev_num: Some(0) })?;
+    fn test_dev_ordering() -> Result<()> {
+        test_a_greater(DevHead { dev_num: Some(0) }, DevHead { dev_num: None })?;
+        test_a_greater(DevHead { dev_num: Some(1) }, DevHead { dev_num: Some(0) })?;
         Ok(())
     }
 
     #[test]
-    fn check_pep440_equality() -> Result<()> {
+    fn test_pep440_equality() -> Result<()> {
         assert_eq!(
             PackageVersion::new("1.0a1")?,
             PackageVersion::new("1.0alpha1")?
@@ -471,7 +472,7 @@ mod tests {
     }
 
     #[test]
-    fn check_pep440() {
+    fn test_pep440() {
         // list of every example mentioned in pep-440
         let versions = vec![
             "1.0",
@@ -513,7 +514,7 @@ mod tests {
     }
 
     #[test]
-    fn check_pep440_negative() {
+    fn test_pep440_negative() {
         let versions = vec!["not a version"];
 
         for version in versions {
